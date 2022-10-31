c764465cf32ff1f95e5c1e61229974ef381fc88b
Diryon Mora <oktuvida@hotmail.com>
2022-10-30 23:29:57 -0500

    Wave UI component added

--- src/components/UI/Wave.tsx
+++ src/components/UI/Wave.tsx
@@ -1,30 +1,45 @@
-import React from "react";
-import { Dimensions, StyleSheet, View, ViewStyle } from "react-native";
+import React, { useMemo } from "react";
+import { Platform, StyleSheet, View, ViewStyle } from "react-native";
 import { useTheme } from "react-native-paper";
 import Svg, { Path } from "react-native-svg";
+import useSessionStore from "@hooks/useSessionStore";
 import { contrastColor } from "@resources/colors";
 
 export type WaveProps = {
   contrastAmount?: number;
   opacity?: number;
+  height?: number;
   style?: ViewStyle;
 };
+
 export default React.memo(function Wave({
-  contrastAmount,
-  opacity,
+  contrastAmount = 10,
+  opacity = 1,
+  height = 300,
   style,
 }: WaveProps) {
   const theme = useTheme();
-  const { colors } = theme;
+  const windowWidth = useSessionStore((state) => state.windowWidth);
+
+  const fillColor = useMemo(() => {
+    return contrastColor(theme.colors.primary, contrastAmount);
+  }, [theme.colors.primary]);
+  const styles = useMemo(() => {
+    return createStyles({
+      fillColor,
+      height,
+      windowWidth,
+    });
+  }, [fillColor, height, windowWidth]);
 
   return (
-    <View style={{ ...style, ...styles.wrapper }}>
+    <View style={[style, styles.wrapper]}>
       <View style={styles.container}>
         <Svg viewBox="0 0 1440 320" style={styles.svg}>
           <Path
-            fill={contrastColor(colors.primary, contrastAmount ?? 0)}
-            fillOpacity={opacity ?? 1}
-            d="M0,0L21.8,37.3C43.6,75,87,149,131,165.3C174.5,181,218,139,262,138.7C305.5,139,349,181,393,186.7C436.4,192,480,160,524,144C567.3,128,611,128,655,128C698.2,128,742,128,785,160C829.1,192,873,256,916,261.3C960,267,1004,213,1047,197.3C1090.9,181,1135,203,1178,197.3C1221.8,192,1265,160,1309,149.3C1352.7,139,1396,149,1418,154.7L1440,160L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"
+            fill={fillColor}
+            fillOpacity={opacity}
+            d="M0,64L34.3,58.7C68.6,53,137,43,206,42.7C274.3,43,343,53,411,90.7C480,128,549,192,617,229.3C685.7,267,754,277,823,256C891.4,235,960,181,1029,144C1097.1,107,1166,85,1234,80C1302.9,75,1371,85,1406,90.7L1440,96L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"
           />
         </Svg>
       </View>
@@ -32,20 +47,30 @@ export default React.memo(function Wave({
   );
 });
 
-const styles = StyleSheet.create({
-  wrapper: {
-    position: "absolute",
-    width: Dimensions.get("window").width,
-    minHeight: Dimensions.get("window").height,
-    height: "100%",
-  },
-  container: {
-    width: "100%",
-    height: "100%",
-  },
-  svg: {
-    position: "absolute",
-    width: "100%",
-    bottom: 0,
-  },
-});
+const createStyles = ({
+  fillColor,
+  height,
+  windowWidth,
+}: {
+  fillColor: string;
+  height: number;
+  windowWidth: number;
+}) =>
+  StyleSheet.create({
+    wrapper: {
+      position: "absolute",
+      width: windowWidth,
+      bottom: 0,
+      zIndex: -1000,
+    },
+    container: {
+      backgroundColor: fillColor,
+      height: Math.pow(height, 1.005) * (Platform.OS === "web" ? 0.4 : 1),
+    },
+    svg: {
+      position: "absolute",
+      width: windowWidth,
+      height: windowWidth * 0.225,
+      bottom: height * (Platform.OS === "web" ? 0.3 : 1),
+    },
+  });
