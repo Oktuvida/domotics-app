ec85021df984ae45428d533f11ac53be0f526a1f
Diryon Mora <oktuvida@hotmail.com>
2022-11-12 18:03:39 -0500

    Metro config formatted

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,12 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-const config = getDefaultConfig(__dirname)
-config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
-config.resolver.assetExts = config.resolver.assetExts.filter(ext => !ext.includes("svg"));
+const config = getDefaultConfig(__dirname);
+config.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => !ext.includes("svg")
+);
 config.resolver.sourceExts.push("svg");
 
 module.exports = config;
